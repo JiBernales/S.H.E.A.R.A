93debed5d78b2f5040ffc2f3ac1d2e9f97c4569d
Jezia Sabalilag <jcsabalilag@up.edu.ph>
2024-01-08 23:12:26 +0800

    Modify settings

--- sheara/lib/settings.dart
+++ sheara/lib/settings.dart
@@ -1,128 +1,86 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
   @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool isDarkModeEnabled = false;
+  bool isNotificationEnabled = false;
+  bool isSoundEnabled = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-        centerTitle: true,
-        backgroundColor: Colors.grey,
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            margin:
-                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Form(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    child: const Icon(
-                      Icons.person,
-                      size: 48,
-                      color: Colors.white,
+    return MaterialApp(
+      theme: isDarkModeEnabled ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Settings"),
+          centerTitle: true,
+          backgroundColor: Colors.grey,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Form(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: const Icon(
+                        Icons.settings,
+                        size: 48,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  SwitchListTile(
-                    value: false,
-                    onChanged: null,
-                    title: Text("Dark Mode"),
-                  ),
-                  SwitchListTile(
-                    value: false,
-                    onChanged: null,
-                    title: Text("Notification"),
-                  ),
-                  const SizedBox(height: 10),
-                  SwitchListTile(
-                    value: false,
-                    onChanged: null,
-                    title: Text("Sound"),
-                  ),
-                  const SizedBox(height: 10),
-                  Container(
-                    color: Theme.of(context).canvasColor,
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: <Widget>[
-                        const Text(
-                          "Personal Information",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ),
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: "Student Number",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15.0)),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: "Username",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15.0)),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15.0)),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: "Parent's Contact Number",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15.0)),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: "Guardian's Contact Number",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15.0)),
-                            ),
-                          ),
-                        ),
-                      ],
+                    const SizedBox(height: 10),
+                    SwitchListTile(
+                      value: isDarkModeEnabled,
+                      onChanged: (value) {
+                        setState(() {
+                          isDarkModeEnabled = value;
+                        });
+                      },
+                      title: const Text("Dark Mode"),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Handle save logic
-                    },
-                    child: const Text("Save"),
-                  ),
-                ],
+                    SwitchListTile(
+                      value: isNotificationEnabled,
+                      onChanged: (value) {
+                        setState(() {
+                          isNotificationEnabled = value;
+                        });
+                      },
+                      title: const Text("Notification"),
+                    ),
+                    const SizedBox(height: 10),
+                    SwitchListTile(
+                      value: isSoundEnabled,
+                      onChanged: (value) {
+                        setState(() {
+                          isSoundEnabled = value;
+                        });
+                      },
+                      title: const Text("Sound"),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
               ),
             ),
           ),
