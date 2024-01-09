bf984549107fa74964b7c2974f626861126cdf94
Jezia Bethany Sabalilag <70994577+bitany@users.noreply.github.com>
2024-01-09 10:39:20 +0800

    Added the developers and their roles

--- sheara/lib/about_page.dart
+++ sheara/lib/about_page.dart
@@ -5,52 +5,37 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('About Health Emergency App'),
-        backgroundColor: Colors.redAccent,
+        title: Text(
+          'The Developers',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Color.fromARGB(255, 194, 88, 23),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Welcome to the Health Emergency and Response App',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
             SizedBox(height: 20),
-            Text(
-              'About Us',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            Center(
+              child: Text(
+                'MEET THE TEAM',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            Text(
-              'Our mission is to provide a user-friendly and effective mobile application for handling health emergencies and responses. We understand the importance of quick and reliable assistance in critical situations.',
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Key Features',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '1. Emergency Contacts: Easily access and call local emergency services such as hospitals, ambulance services, and more.',
-            ),
-            Text(
-              '2. Quick Assistance: Notify your emergency contacts with your current location and status in just a few taps.',
-            ),
-            Text(
-              '3. Medical Information: Store essential medical information for quick reference by healthcare professionals.',
-            ),
-            Text(
-              '4. Emergency Tips: Get valuable information and tips on handling common health emergencies.',
-            ),
+            _buildTeamMember('assets/hadjie.png', 'HADJIE BERNALES JR',
+                'Scrum Master/Project Manager'),
+            _buildTeamMember('assets/maxinne.png', 'MAXINNE GWEN CAHILIG',
+                'Documentation Specialist'),
+            _buildTeamMember(
+                'assets/cate.png', 'CATE GELLAI MUCAS', 'Frontend Developer'),
+            _buildTeamMember(
+                'assets/kyle.png', 'JOHN KYLE JUNSAY', 'Backend Developer'),
+            _buildTeamMember('assets/jezia.png', 'JEZIA BETHANY SABALILAG',
+                'Frontend Developer'),
             SizedBox(height: 20),
             Text(
               'Contact Us',
@@ -71,6 +56,29 @@ class AboutPage extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTeamMember(String imagePath, String name, String role) {
+    return Row(
+      children: [
+        Image.asset(imagePath,
+            width: 80, height: 80), // Adjust width and height as needed
+        SizedBox(width: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              name,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(role),
+          ],
+        ),
+      ],
     );
   }
 }
